1dcf9433c1a65865ab396c91fcef3c2f32f194bc
isolaee <eero.isola@gmail.com>
2026-01-16 13:24:52 +0200

    reduces styling

--- templates/myaccount/yhteydenotot.php
+++ templates/myaccount/yhteydenotot.php
@@ -5,11 +5,5 @@
 
 defined('ABSPATH') || exit;
 
-?>
-<div class="woocommerce-yhteydenotot">
-    <?php 
-    // Render shortcodes directly
-    echo do_shortcode('[cf7-views id="2369"]');
-    echo do_shortcode('[cf7-views id="2374"]');
-    ?>
-</div>
+echo do_shortcode('[cf7-views id="2369"]');
+echo do_shortcode('[cf7-views id="2374"]');
