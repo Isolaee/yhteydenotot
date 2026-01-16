4b2d5df4a821816598afd594880667c3ade15a27
isolaee <eero.isola@gmail.com>
2026-01-16 13:04:26 +0200

    echo do_shortcode directly

--- templates/myaccount/yhteydenotot.php
+++ templates/myaccount/yhteydenotot.php
@@ -5,11 +5,11 @@
 
 defined('ABSPATH') || exit;
 
-// Ensure shortcodes are loaded
-do_action('wp_loaded');
-
 ?>
 <div class="woocommerce-yhteydenotot">
-    <?php echo apply_filters('the_content', '[cf7-views id="2369"]'); ?>
-    <?php echo apply_filters('the_content', '[cf7-views id="2374"]'); ?>
+    <?php 
+    // Render shortcodes directly
+    echo do_shortcode('[cf7-views id="2369"]');
+    echo do_shortcode('[cf7-views id="2374"]');
+    ?>
 </div>
