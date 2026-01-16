202b7c7e1ec4be4fe3a3a03a6184886da5e1b1bd
isolaee <eero.isola@gmail.com>
2026-01-16 13:00:21 +0200

    Refactor shortcode rendering to use apply_filters and ensure shortcodes are loaded

--- templates/myaccount/yhteydenotot.php
+++ templates/myaccount/yhteydenotot.php
@@ -5,5 +5,11 @@
 
 defined('ABSPATH') || exit;
 
-echo do_shortcode('[cf7-views id="2369"]');
-echo do_shortcode('[cf7-views id="2374"]');
+// Ensure shortcodes are loaded
+do_action('wp_loaded');
+
+?>
+<div class="woocommerce-yhteydenotot">
+    <?php echo apply_filters('the_content', '[cf7-views id="2369"]'); ?>
+    <?php echo apply_filters('the_content', '[cf7-views id="2374"]'); ?>
+</div>
